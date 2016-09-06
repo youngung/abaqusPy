ebdd4479cd5c8f6c1b7cadaed958362541744a26
Youngung Jeong <younguj@clemson.edu>
2016-09-06 16:58:14 -0400

    Snapshot

--- uni.py
+++ uni.py
@@ -16,11 +16,11 @@ backwardCompatibility.setValues(includeDeprecated=True,
 
 import sketch
 import part
+import regionToolset
 import numpy as np
 
 
 ###### ----------Generating specimen dimensions.
-
 def drawE8_Sketch(
     pl=57,   ## Parallel length
     gw=12.5, ## Gauge width
@@ -31,8 +31,7 @@ def drawE8_Sketch(
     """
     Draw E8 uniaxial tension specimen
     Units are in [mm]
-    
-    
+
     Arguments
     ---------
     pl : parallel length  (57   mm)
@@ -54,39 +53,36 @@ def drawE8_Sketch(
     ths=[th0*np.pi/180.,th1*np.pi/180.]
     xs = rd*np.cos(ths)
     ys = rd*np.sin(ths)
-
     ## translate xs,ys
     xs = xs + (x-xs[-1])
     ys = ys + (-A+tw-ys[-1])
     xyRound=[xs.tolist(),ys.tolist()]
-    
     ## parallel
     x0,y0=xs[0],ys[0]
     xyParallel = [[x0-0.5*pl,x0],[y0,y0]]
-    
     ## Right grip
     XS=[x+tl,x+tl,x][::-1]
     YS=[-A+0.5*tw,-A+tw,-A+tw][::-1]
     xyRG=[XS,YS]
-    
+
     x=xyParallel[0]+xyRound[0]+xyRG[0]
     y=xyParallel[1]+xyRound[1]+xyRG[1]
-    
+
     xyCoords=np.array([x,y])
-    
+
     # print xyCoords.shape
-    
+
     ## translate the coordinate so that the center of gravity is (0,0)
     xyCoords[0]=xyCoords[0]-xyCoords[0][0]
     xyCoords[1]=xyCoords[1]-xyCoords[1][-1]
     # plot(xyCoords[0],xyCoords[1],'-')
-    
+
     ## Apply 2-fold symmetry.
     sym0 =[[ 1,0],[0, 1]] ## Identical
     sym1 =[[-1,0],[0, 1]] ## Reflect y axis
     sym2 =[[ 1,0],[0,-1]] ## Reflect x axis
     sym3 =[[-1,0],[0,-1]] ## Reflect origin
-    
+
     sym = np.array([sym0,sym2,sym3,sym1])
     # plot(xyCoords[0,0],xyCoords[1,0],'x')
 
@@ -109,10 +105,10 @@ def drawE8_Sketch(
 
     x0=min(xyTot[0])
     y0=min(xyTot[1])+tw/2.
-    
+
     xyTot[0] = xyTot[0] - x0
     xyTot[1] = xyTot[1] - y0
-    
+
     return xyTot
 
 # ---
@@ -121,7 +117,7 @@ def tensileBar(pl=57,   ## Parallel length
                tw=20,   ## Grip width
                tl=50.0, ## Grip length
                rd=12.5, ## Radius
-               ):                   
+               ):
     """
     Close end.
     """
@@ -137,7 +133,7 @@ def tensileBar(pl=57,   ## Parallel length
             pass
         else:
             XYS.append([x1,y1])
-            
+
     XYS.append(XYS[0])
     xyt=np.array(XYS).T
     return xyt
@@ -153,18 +149,13 @@ xyCoords = tensileBar(pl=pl,gw=gw,tw=tw,tl=tl,rd=rd).T
 
 myModel = mdb.Model(name='UniaxialTension')
 mySketch = myModel.ConstrainedSketch(name='E8_Sketch',sheetSize=1.0)
-    
-#s.ArcByCenterEnds(center=?, point1=?,point2?)
 
+#s.ArcByCenterEnds(center=?, point1=?,point2?)
 totalLength=np.max(xyCoords[:,0])-np.min(xyCoords[:,0]) ## total length of specimen.
 
 for i in xrange(len(xyCoords)):
     mySketch.Spot(point=xyCoords[i])
 
-#for i in xrange(len(xyCoords)-1):
-#    mySketch.Line(point1=(xyCoords[i,0],xyCoords[i,1]),
-#                  point2=(xyCoords[i+1,0],xyCoords[i+1,1]))
-                  
 g,v,d,c=mySketch.geometry, mySketch.vertices, mySketch.dimensions, mySketch.constraints
 
 def draw_arc(iref,i0,i1,opt='up',):
@@ -177,7 +168,7 @@ def draw_arc(iref,i0,i1,opt='up',):
     opt : 'up' or 'down' depending on the reletive location of circle center to specimen.
     """
     xyArc=xyCoords[iref]
-    
+
     if opt=='up':    centerArc = (xyArc[0],xyArc[1]+gw)
     elif opt=='down':centerArc = (xyArc[0],xyArc[1]-gw)
     else: raise IOError, 'Unexpected opt given'
@@ -185,7 +176,7 @@ def draw_arc(iref,i0,i1,opt='up',):
     p1=(xyCoords[i0][0],xyCoords[i0][1])
     p2=(xyCoords[i1][0],xyCoords[i1][1])
     mySketch.ArcByCenterEnds(center=centerArc, point1=p1, point2=p2)
-    
+
 def draw_line(i0,i1):
     n=i1-i0
     for i in xrange(n):
@@ -193,7 +184,7 @@ def draw_line(i0,i1):
         I1=I0+1
         p1=(xyCoords[I0][0],xyCoords[I0][1])
         p2=(xyCoords[I1][0],xyCoords[I1][1])
-        
+
         mySketch.Line(point1=p1,point2=p2)
 
 draw_arc(0,0,1,'up')
@@ -210,8 +201,6 @@ draw_line(18,20)
 ###### ----------Generating specimen dimensions.
 myPart = myModel.Part(name='E8', dimensionality=THREE_D,type=DEFORMABLE_BODY)
 
-
-
 def findEdgeBetween(i0,i1):
     x0,y0=xyCoords[i0]
     x1,y1=xyCoords[i1]
@@ -222,17 +211,12 @@ def findEdgeBetween(i0,i1):
 
 ## Features.
 featShell=myPart.BaseShell(sketch=mySketch)
+myPart.features.changeKey(fromName=featShell.name,toName='myBaseShell')
 session.viewports['Viewport: 1'].setValues(displayedObject=myPart)
-                  
-
-#myPart.edges.findat
-#edge0=findEdgeBetween(12,13)
-#edge1=findEdgeBetween(14,15)
 
 edge0=myPart.edges.findAt((0,0.0001,0))
 edge1=myPart.edges[edge0.index-1:edge0.index+1]
 myPart.Set(edges=edge1,name='leftEnd')
-
 
 xyt=np.array(xyCoords).T
 xs=xyt[0]
@@ -240,65 +224,184 @@ xMax=np.max(xs)
 edge0=myPart.edges.findAt((xMax,0.0001,0))
 edge1=myPart.edges[edge0.index:edge0.index+2]
 myPart.Set(edges=edge1,name='rightEnd')
+# Find length of round
+roundLength=(xMax-tl*2.-pl)/2.
 
 
 ## create datum for material orientation
 # - create three datum points
 # assuming that x//RD, y//TD and z//ND
-# x
+def returnDatumCoords(coords):
+    ## below returns a feature
+    f=myPart.DatumPointByCoordinate(coords=coords)
+    ## convert feature to datum
+    return myPart.datums[f.id]
+rDC=returnDatumCoords # alias
 
-dato=myPart.DatumPointByCoordinate(coords=(0,    0,    0))
-datx=myPart.DatumPointByCoordinate(coords=(tw/2.,0,    0))
-daty=myPart.DatumPointByCoordinate(coords=(0.   ,tw/2.,0))
+datOri=rDC(coords=(0,0,0))
+datO=rDC(coords=(totalLength      ,0,    0))
+datX=rDC(coords=(totalLength-tw/2.,0,    0))
+datY=rDC(coords=(totalLength      ,tw/2.,0))
+datC=rDC(coords=(totalLength/2., tw/2.,0))
+datC_up=rDC(coords=(totalLength/2., tw/2.+gw/2.,0))
+datC_down=rDC(coords=(totalLength/2., tw/2.-gw/2.,0))
+datC_Lend=rDC(coords=(0,tw/2.,0))
+datC_Rend=rDC(coords=(totalLength,tw/2.,0))
 
+datC_Left=rDC(coords=(totalLength/2.-pl/2., tw/2.,0))
+datC_LeftUp=rDC(coords=(totalLength/2.-pl/2., tw/2.+gw/2.,0))
+datC_LeftDown=rDC(coords=(totalLength/2.-pl/2., tw/2.-gw/2.,0))
 
-datO=myPart.DatumPointByCoordinate(coords=(totalLength      ,0,    0))
-datX=myPart.DatumPointByCoordinate(coords=(totalLength-tw/2.,0,    0))
-datY=myPart.DatumPointByCoordinate(coords=(totalLength      ,tw/2.,0))
+datC_Right=rDC(coords=(totalLength/2.+pl/2., tw/2.,0))
+datC_RightUp=rDC(coords=(totalLength/2.+pl/2., tw/2.+gw/2.,0))
+datC_RightDown=rDC(coords=(totalLength/2.+pl/2., tw/2.-gw/2.,0))
 
-datC=myPart.DatumPointByCoordinate(coords=(totalLength/2., tw/2.,0))
-datC_up=myPart.DatumPointByCoordinate(coords=(totalLength/2., tw/2.+gw/2.,0))
-datC_down=myPart.DatumPointByCoordinate(coords=(totalLength/2., tw/2.-gw/2.,0))
+## Define Point Datums at the endge of Grip.
+datPs=np.empty((2,3),dtype='object')
+xs=[tl,tl+pl+2.*roundLength]
+ys=[0,tw/2.,tw]
+for i in xrange(2):
+    for j in xrange(3):
+        datPs[i,j] = rDC(coords=(xs[i],ys[j],0))
 
-datC_Left=myPart.DatumPointByCoordinate(coords=(totalLength/2.-pl/2., tw/2.,0))
-datC_LeftUp=myPart.DatumPointByCoordinate(coords=(totalLength/2.-pl/2., tw/2.+gw/2.,0))
-datC_LeftDown=myPart.DatumPointByCoordinate(coords=(totalLength/2.-pl/2., tw/2.-gw/2.,0))
-
-datC_Right=myPart.DatumPointByCoordinate(coords=(totalLength/2.+pl/2., tw/2.,0))
-datC_RightUp=myPart.DatumPointByCoordinate(coords=(totalLength/2.+pl/2., tw/2.+gw/2.,0))
-datC_RightDown=myPart.DatumPointByCoordinate(coords=(totalLength/2.+pl/2., tw/2.-gw/2.,0))
-
-cSysDefault = myPart.DatumCsysByDefault(CARTESIAN)
-
-
-## renaming datums.
-myPart.features.changeKey(fromName=cSysDefault.name,toName='cSysDefault')
-
-
-#myPart.DatumCsysByOffset(coordSysType=CARTESIAN, datumCoordSys=cSysDefault, point=((totalLength/2.,0.,0.),))
-
-
-#e=myPart.edges
-# two lines for CSYSM
-#l1=e.findAt((0.001,0,0),)
-#l2=e.findAt((0,0.001,0),)
-#myPart.DatumCsysByTwoLines(CARTESIAN,line1=l1,line2=l2)
+#datP=np.array(dtype='obj')
 
 
-#myPart.Set(edges=(edge0,edge1),name='leftEnd')
-#myPart.edges.getByBoundingBox(xMax=-0.1, xMin=1,yMin=-0.1, yMax=1, zMin=-0.1,zMax=1.)
+#cSysDefault = myPart.DatumCsysByDefault(CARTESIAN)
+cSysMat     = myPart.DatumCsysByThreePoints(CARTESIAN,origin=datC,point1=datC_Right,point2=datC_up,name='matCsys')
+
+## Shell
+myPart.BaseShell(sketch=mySketch)
+session.viewports['Viewport: 1'].setValues(displayedObject=myPart)
 
 
-#myPart.BaseSolidExtrude(sketch=mySketch,depth=20)
+## Create My material
+myMat=myModel.Material('Metal')
+myMat.Elastic(table=((30.e9,0.3),))
+
+
+## Create Shell Section!
+myShellSection=myModel.HomogeneousShellSection(
+    name='SpecimenSection',preIntegrate=ON,
+    material='Metal',thickness=1.e-3,poissonDefinition=DEFAULT, temperature=GRADIENT)
+
+
+## Assign material orientation
+myPart.MaterialOrientation(localCsys=cSysMat,axis=AXIS_3)
+
+## Assign section to plate
+region=regionToolset.Region(faces=myPart.faces[:])
+myPart.SectionAssignment(region=region,sectionName=myShellSection.name)
+
+
+## retrieve assembly
+myAssembly = myModel.rootAssembly
+session.viewports['Viewport: 1'].setValues(displayedObject=myAssembly)
+# Set cSys to myAssembly
+myAssembly.DatumCsysByDefault(CARTESIAN)
+# Instance the part E8
+myAssembly.Instance(name='MySpecimen', part=myPart, dependent=ON)
+myInstance=myAssembly.instances['MySpecimen']
+
+
+## Partition the plate
+#- transPlane  -  along transverse direction to the specimen axis
+transPlane=myPart.PartitionFaceByShortestPath(faces=myPart.faces[:],point1=datC_up,point2=datC_down)
+myPart.features.changeKey(fromName=transPlane.name,toName='transPlane')
+#- axialPlane  -  along longitudinal direction of the specimen.
+axialPlane=myPart.PartitionFaceByShortestPath(faces=myPart.faces[:],point1=datC_Lend,point2=datC_Rend)
+myPart.features.changeKey(fromName=axialPlane.name,toName='axialPlane')
+#- pLeftPlane
+transPlane=myPart.PartitionFaceByShortestPath(faces=myPart.faces[:],point1=datC_LeftDown,point2=datC_LeftUp)
+myPart.features.changeKey(fromName=transPlane.name,toName='pLeftPlane')
+#- pRightPlane
+transPlane=myPart.PartitionFaceByShortestPath(faces=myPart.faces[:],point1=datC_RightDown,point2=datC_RightUp)
+myPart.features.changeKey(fromName=transPlane.name,toName='pRightPlane')
+
+
+## MidSpan
+SpecimenNameInAssembly=myAssembly.instances.items()[0][0]
+edges=myAssembly.instances[SpecimenNameInAssembly].edges
+
+# Assign MidSpan using datum called datC: which is located in the center of specimen
+def setTransSpan(dat,name):
+    coord1=np.array(dat.pointOn)
+    coord2=np.array(dat.pointOn)
+    coord1[1]=coord1[1]-tw/4.
+    coord2[1]=coord2[1]+tw/4.
+    C1=tuple(coord1);C2=tuple(coord2)
+    myAssembly.Set(edges=(edges.findAt((C1,),(C2,))), name=name)
+
+setTransSpan(dat=datC,name='MidSpan')
+setTransSpan(dat=datC_Left,name='ParallelLeft')
+setTransSpan(dat=datC_Right,name='ParallelRight')
+
+#coord1=np.array(datC.pointOn)
+#coord2=np.array(datC.pointOn)
+#coord1[1]=coord1[1]-tw/4.
+#coord2[1]=coord2[1]+tw/4.
+#C1=tuple(coord1);C2=tuple(coord2)
+#myAssembly.Set(edges=(edges.findAt((C1,),(C2,))), name='MidSpan')
+
+# Start of parallel
+#coord1=np.array(datC
+# End of parallel
 
 
 
 
+## Create a static general step
+myModel.StaticStep(name='Tension',previous='Initial',description='Uniaxial tension', timePeriod=1,adiabatic=OFF,maxNumInc=100,
+                   stabilization=None,timeIncrementationMethod=AUTOMATIC, initialInc=1,minInc=1e-5,maxInc=1,
+                   matrixSolver=SOLVER_DEFAULT,extrapolation=DEFAULT)
+## view
+session.viewports['Viewport: 1'].assemblyDisplay.setValues(step='Tension')
+
+
+## Modify output request
+# Field output
+myModel.fieldOutputRequests['F-Output-1'].setValues(variables=('S','E','U',))
+# History output
+myModel.historyOutputRequests['H-Output-1'].setValues(variables=('U3',),region=myAssembly.sets['MidSpan'])
+
+session.viewports['Viewport: 1'].assemblyDisplay.setValues(loads=ON, bcs=ON,
+    predefinedFields=ON)
+
+
+## Apply BC
+c0=datOri.pointOn
+vs=myInstance.vertices.findAt((c0,))
+myModel.EncastreBC(name='EncastreOri',createStepName='Tension',region=regionToolset.Region(vertices=vs))
+
+myModel.XsymmBC(name='FixLeftEndX', createStepName='Tension',region=myInstance.sets['leftEnd'])
+myModel.ZsymmBC(name='FixLeftEndZ', createStepName='Tension',region=myInstance.sets['leftEnd'])
+myModel.XsymmBC(name='FixRightEndZ',createStepName='Tension',region=myInstance.sets['rightEnd'])
+
+# Velocity
+myModel.VelocityBC(name='StretchX', createStepName='Tension',region=myInstance.sets['rightEnd'])
+
+
+
+## Mesh
+elemType1 = mesh.ElemType(elemCode=S4R, elemLibrary=STANDARD)
+elemType2 = mesh.ElemType(elemCode=S3, elemLibrary=STANDARD)
+f = myPart.faces
+faces = f.getSequenceFromMask(mask=('[#f ]', ), )
+pickedRegions =(faces, )
+myPart.setElementType(regions=pickedRegions, elemTypes=(elemType1, elemType2))
+myPart.seedPart(size=0.002, minSizeFactor=0.1)
+myPart.generateMesh()
 
 
 
 
+# mdb.meshEditOptions.setValues(enableUndo=True, maxUndoCacheElements=0.5)
+# myPart.generateMesh()
 
 
+# #c0=datOri.pointOn
+# #vs=myInstance.vertices.findAt((c0,))
+# #myModel.YsymmBC(name='Yfix',createStepName='Tension',region=regionToolset.Region(vertices=vs))
+# #myModel.ZsymmBC(name='Zfix',createStepName='Tension',region=regionToolset.Region(vertices=vs))
 
 
