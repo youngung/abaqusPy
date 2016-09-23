f1bc19ce91221a56b40306a0e6382f1c2be6c65a
Youngung Jeong <younguj@clemson.edu>
2016-09-23 17:15:56 -0400

    iload add to describe the loading condition for one element

--- examples/one/one.py
+++ examples/one/one.py
@@ -46,12 +46,38 @@ def square(length=1.e-3):
     xyCoords=np.array(xyCoords) * length
     return xyCoords
 
+def set_EncastreOri(
+    myModel=None,myInstance=None,dat=None,
+    createStepName='Initial',name='None'):
+    """
+    Arguments
+    ---------
+    myModel        = Abaqus Model
+    dat            = point datum
+    createStepName = 'initial'
+    """
+    c0=dat.pointOn
+    vs=myInstance.vertices.findAt((c0,))
+    ## Encastre
+    myModel.EncastreBC(name=name,createStepName=createStepName,
+                       region=regionToolset.Region(vertices=vs))
+
+def add_user_orien(myPart,region):
+    """
+    Add user orietation to the defined <region> pertaining to <myPart>
+    """
+    myPart.MaterialOrientation(
+        region=region, orientationType=USER,
+        additionalRotationType=ROTATION_NONE,
+        localCsys=None, fieldName='')
+
+
 length=1.e-3
 xyCoords=square(length=length)
 
 def TensileOneElement(
     Theta=0.,isub=False,iwait=False,
-    umatFN=None,myMatFunc=None,totalStrain=0.05):
+    umatFN=None,myMatFunc=None,totalStrain=0.05,iload=0):
     """
     Prep one shell element tests
 
@@ -64,6 +90,10 @@ def TensileOneElement(
     umatFN      = None
     myMatFunc   = None
     totalStrain = 0.05
+    iload       = 0:  uniaxial along X
+                  1:  pure shear
+                  2:  simple shear
+                  3:  uniaxial along Y
     """
     ### Model declaration
     gpa=1.e9
@@ -108,14 +138,6 @@ def TensileOneElement(
                               toName='myBaseShell')
     session.viewports['Viewport: 1'].setValues(displayedObject=myPart)
 
-    ### Edge sets pertaining to Part
-    edge0=myPart.edges.findAt((0,0.0001,0))
-    edges=myPart.edges[edge0.index:edge0.index+1]
-    myPart.Set(edges=edges,name='leftEnd')
-    edge0=myPart.edges.findAt((length,0.0001,0))
-    edges=myPart.edges[edge0.index:edge0.index+1]
-    myPart.Set(edges=edges,name='rightEnd')
-
     ### Define various point Datums
     ## Coordinate datum for material orientation
     ## assuming that x//RD, y//TD and z//ND
@@ -124,11 +146,31 @@ def TensileOneElement(
     datRC = rPDC(myPart, coords=(length,   length/2.,    0))
     dat11 = rPDC(myPart, coords=(length,      length,    0))
     datC  = rPDC(myPart, coords=(length/2.,length/2.,    0))
-
     ### Define Point Datums at the two edges of Grips.
     SysDefault = myPart.DatumCsysByDefault(CARTESIAN)
     cSysMat = abaquspy.lib.datums.returnCsymPlanarAngle(
         myPart,datC,radius=length/3.,angle=Theta*np.pi/180.)
+    dat01  = rPDC(myPart, coords=(0,length,    0))
+
+    ### Edge sets pertaining to Part
+    edge0=myPart.edges.findAt((0,0.0001,0))
+    edges=myPart.edges[edge0.index:edge0.index+1]
+    myPart.Set(edges=edges,name='leftEnd')
+    edge0=myPart.edges.findAt((length,0.0001,0))
+    edges=myPart.edges[edge0.index:edge0.index+1]
+    myPart.Set(edges=edges,name='rightEnd')
+    edge0=myPart.edges.findAt((0.0001,length,0))
+    edges=myPart.edges[edge0.index:edge0.index+1]
+    myPart.Set(edges=edges,name='upEnd')
+    edge0=myPart.edges.findAt((0.0001,0,0))
+    edges=myPart.edges[edge0.index:edge0.index+1]
+    myPart.Set(edges=edges,name='downEnd')
+    vts = myPart.vertices.findAt(dat11.pointOn)
+    vts = myPart.vertices[vts.index:vts.index+1]
+    myPart.Set(vertices=vts,name='v11')
+    vts = myPart.vertices.findAt(dat01.pointOn)
+    vts = myPart.vertices[vts.index:vts.index+1]
+    myPart.Set(vertices=vts,name='v01')
 
     ### Shell
     myPart.BaseShell(sketch=mySketch)
@@ -137,7 +179,6 @@ def TensileOneElement(
     ### Create My material
     ## Use abaquspy.mats.ifsteel module to retrieve material
     ## characteristics of the IF steel
-
 
     if type(umatFN)==type(None):
         myMat = myModel.Material('IFsteel') ## modules:
@@ -161,20 +202,6 @@ def TensileOneElement(
     myShellSection.TransverseShearShell(
         k11=200.*gpa,k22=200.*gpa,k12=120.*gpa)
 
-
-    # print '** Material orientation specification **'
-    # region = regionToolset.Region(faces=myPart.faces[:])
-    # myPart.MaterialOrientation(
-    #     region=region,
-    #     orientationType=SYSTEM,
-    #     axis=AXIS_3,
-    #     localCsys=cSysMat,
-    #     fieldName='',
-    #     additionalRotationType=ROTATION_NONE,
-    #     angle=0,
-    #     additionRotationField='')
-    # print '**                                    **'
-
     ### Assign section to plate
     region=regionToolset.Region(faces=myPart.faces[:])
     myPart.SectionAssignment(region=region,sectionName=myShellSection.name)
@@ -188,22 +215,17 @@ def TensileOneElement(
     myAssembly.Instance(name='MySpecimen', part=myPart, dependent=ON)
     myInstance=myAssembly.instances['MySpecimen']
 
-
     ### Assign material orientation
     region = regionToolset.Region(faces=myPart.faces[:])
-    orientation = mdb.models[myModelName].parts[myPartName].datums[12]
+    orientation =  mdb.models[myModelName].parts[myPartName].datums[cSysMat.id]
     mdb.models[myModelName].parts[myPartName].MaterialOrientation(
-    region=region, orientationType=SYSTEM, axis=AXIS_3, localCsys=orientation,
-    fieldName='', additionalRotationType=ROTATION_NONE, angle=0.0,
-    additionalRotationField='')
+        region=region, orientationType=SYSTEM, axis=AXIS_3,
+        localCsys=orientation,fieldName='',
+        additionalRotationType=ROTATION_NONE, angle=0.0,
+        additionalRotationField='')
+    ## user defined orientation
 
 
-    ### Create a static general step
-    #myModel.StaticStep(
-    #    name='Tension',previous='Initial',description='Uniaxial tension',
-    #    timePeriod=1,adiabatic=OFF,maxNumInc=100,stabilization=None,
-    #    timeIncrementationMethod=AUTOMATIC,initialInc=1,minInc=1e-5,
-    #    maxInc=1,matrixSolver=SOLVER_DEFAULT,extrapolation=DEFAULT)
 
     ### Define boundary conditions...
     epsRate=1e-3 #0.001/sec
@@ -214,7 +236,6 @@ def TensileOneElement(
     vel=epsRate*L0 ## velocity
 
     ### total (engi) strain wanted: 0.02
-    # totalStrain = 0.20
     Lf=(1.+totalStrain)*L0
     totalDisplace=Lf-L0
     deltaTime=totalDisplace/vel ## total delta Time
@@ -233,32 +254,69 @@ def TensileOneElement(
 
     ## Modify output request
     # Field output
-
     if type(umatFN)==type(None):
         myModel.fieldOutputRequests['F-Output-1'].setValues(
-            variables=('E','U','S','EE','PE'))
+            variables=('E','U','S','EE','PE'),directions=OFF)
     else:
         myModel.fieldOutputRequests['F-Output-1'].setValues(
-            variables=('E','U','S','SDV'))
+            variables=('E','U','S','SDV'),directions=OFF)
 
     ## Apply BC
-    c0=datOri.pointOn
-    vs=myInstance.vertices.findAt((c0,))
     ## Encastre
-    myModel.EncastreBC(name='EncastreOri',createStepName='Initial',
-                       region=regionToolset.Region(vertices=vs))
-    ## Symmetric constraints
-    myModel.XsymmBC(name='FixLeftEndX', createStepName='Initial',
-                    region=myInstance.sets['leftEnd'])
-    myModel.ZsymmBC(name='FixLeftEndZ', createStepName='Initial',
-                    region=myInstance.sets['leftEnd'])
-    myModel.ZsymmBC(name='FixRightEndZ',createStepName='Initial',
-                    region=myInstance.sets['rightEnd'])
-    ## Velocity
-    myModel.VelocityBC(name='StretchX', createStepName='Initial',
-                       region=myInstance.sets['rightEnd'])
-    myModel.boundaryConditions['StretchX'].setValuesInStep(
-        stepName='TensionContinue',v1=vel,vr3=0.)
+    if iload==0: ## uniaxial loading along global direction X
+        set_EncastreOri(myModel,myInstance,datOri,'Initial','fix00')
+        myModel.XsymmBC(name='FixLeftEndX', createStepName='Initial',
+                        region=myInstance.sets['leftEnd'])
+        myModel.ZsymmBC(name='FixLeftEndZ', createStepName='Initial',
+                        region=myInstance.sets['leftEnd'])
+        myModel.ZsymmBC(name='FixRightEndZ',createStepName='Initial',
+                        region=myInstance.sets['rightEnd'])
+        ## Velocity
+        myModel.VelocityBC(name='StretchX', createStepName='Initial',
+                           region=myInstance.sets['rightEnd'])
+        myModel.boundaryConditions['StretchX'].setValuesInStep(
+            stepName='TensionContinue',v1=vel,vr3=0.)
+    elif iload==1:
+        set_EncastreOri(myModel,myInstance,datOri,'Initial','fix00')
+        set_EncastreOri(myModel,myInstance,dat10, 'Initial','fix10')
+        ## Velocity v11
+        myModel.VelocityBC(name='StretchX1', createStepName='Initial',
+                           region=myInstance.sets['v11'])
+        myModel.boundaryConditions['StretchX1'].setValuesInStep(
+            stepName='TensionContinue',v1=vel,vr3=0.)
+        ## Velocity v01
+        myModel.VelocityBC(name='StretchX2', createStepName='Initial',
+                           region=myInstance.sets['v01'])
+        myModel.boundaryConditions['StretchX2'].setValuesInStep(
+            stepName='TensionContinue',v1=vel,vr3=0.)
+    elif iload==2:
+        set_EncastreOri(myModel,myInstance,datOri,'Initial','fix00')
+        set_EncastreOri(myModel,myInstance,dat10, 'Initial','fix10')
+        ## Velocity v11
+        myModel.VelocityBC(name='StretchX1', createStepName='Initial',
+                           region=myInstance.sets['v11'])
+        myModel.boundaryConditions['StretchX1'].setValuesInStep(
+            stepName='TensionContinue',v1=vel,v2=0.,vr3=0.)
+        ## Velocity v01
+        myModel.VelocityBC(name='StretchX2', createStepName='Initial',
+                           region=myInstance.sets['v01'])
+        myModel.boundaryConditions['StretchX2'].setValuesInStep(
+            stepName='TensionContinue',v1=vel,v2=0.,vr3=0.)
+    elif iload==3: ## uniaxial tension along global direction Y
+        set_EncastreOri(myModel,myInstance,datOri,'Initial','fix00')
+        myModel.YsymmBC(name='FixDownEndY', createStepName='Initial',
+                        region=myInstance.sets['downEnd'])
+        myModel.ZsymmBC(name='FixDownEndZ', createStepName='Initial',
+                        region=myInstance.sets['downEnd'])
+        myModel.ZsymmBC(name='FixUpEndZ', createStepName='Initial',
+                        region=myInstance.sets['upEnd'])
+        ## Velocity
+        myModel.VelocityBC(name='StretchY', createStepName='Initial',
+                           region=myInstance.sets['upEnd'])
+        myModel.boundaryConditions['StretchY'].setValuesInStep(
+            stepName='TensionContinue',v2=vel,vr3=0.)
+
+
 
     ## Generate Mesh
     elemType1 = mesh.ElemType(elemCode=S4R, elemLibrary=STANDARD)
@@ -349,26 +407,26 @@ if __name__=='main':
         '-iumat',action='store_true',
         help='whether or not use the umat')
     parser.add_argument(
-        '-iwait',action='store_true',
-        help='iwait')
+        '-iwait',action='store_true',help='iwait')
     parser.add_argument(
         '-mxe',type=float,help='Maximum strain to be imposed',
         default=0.01)
     parser.add_argument(
         '-nth',type=float,help='Number of thetas',
         default=3)
-
+    parser.add_argument(
+        '-iload',type=float,help='Type of loading condition (0: unix, 1: ps, 2: ss, 3:uniy',
+        default=3)
     args = parser.parse_args()
 
-
-    if args.iopt==0:
+    if args.iopt==0: ## run at single material direction
         if args.iumat: umatFN=args.umat
         else: umatFN=None
         runSingle(umatFN=umatFN,
                   iwait=args.iwait,
                   isub=args.isub,
                   totalStrain=args.mxe)
-    elif args.iopt==1:
+    elif args.iopt==1: ## run at multiple number of directions
         if args.iumat: umatFN=args.umat
         else: umatFN=None
         runTensions(umatFN=umatFN,
@@ -378,18 +436,18 @@ if __name__=='main':
                     totalStrain=args.mxe)
 
 
-# ## controlling job conditions
-# #umatFN=None
-# #umatFN='/home/younguj/repo/abaqusPy/umats/el/iso.f'
+### controlling job conditions
+##umatFN=None
+##umatFN='/home/younguj/repo/abaqusPy/umats/el/iso.f'
 umatFN='/home/younguj/repo/abaqusPy/umats/epl/epl.f'
 
-# ## Job testing methods
+### Job testing methods
 # runSingle(umatFN=umatFN,iwait=False,isub=False,totalStrain=0.01)
 
-# ## testing at various angles
-runTensions(nth=3,umatFN=umatFN,isub=False,iwait=False,totalStrain=0.05)
+### testing at various angles
+runTensions(nth=3,umatFN=umatFN,isub=False,iwait=False,totalStrain=0.30)
 
-# #runVarMats(umatFN=None,    isub=True)
-# #runVarMats(umatFN=umatFN,  isub=True)
+##runVarMats(umatFN=None,    isub=True)
+##runVarMats(umatFN=umatFN,  isub=True)
 
 os.sys.path=orig_path[::]
