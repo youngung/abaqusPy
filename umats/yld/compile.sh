91ef65035f413310bc0eed9b9c2ed04d8155ee72
Youngung Jeong <younguj@clemson.edu>
2016-10-04 11:37:33 -0400

    Compiling command for f2py/yld2000-2d

---
+++ umats/yld/compile.sh
@@ -0,0 +1,8 @@
+##
+algb=/home/younguj/repo/abaqusPy/umats/lib/algb.f
+lib_write=/home/younguj/repo/abaqusPy/umats/lib/lib_write.f
+lib=/home/younguj/repo/abaqusPy/umats/lib/lib.f
+is=/home/younguj/repo/abaqusPy/umats/lib/is.f
+cnv=/home/younguj/repo/abaqusPy/umats/lib/cnv.f
+
+f2py -c -m yld2000_2d yld2000_2d.f $algb $lib_write $lib $is $cnv -DF2PY_REPORT_ON_ARRAY_COPY=1.
