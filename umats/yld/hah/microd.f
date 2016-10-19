7f12e413dafb657e95bc91c0be8a193e681191e0
Youngung Jeong <youngung.jeong@gmail.com>
2016-10-19 16:25:23 -0400

    Place-holder for subroutine micro_dev

---
+++ umats/yld/hah/microd.f
@@ -0,0 +1,14 @@
+c-----------------------------------------------------------------------
+c     Microstructure deviator
+
+c     Youngung Jeong
+c     youngung.jeong@gmail.com
+c-----------------------------------------------------------------------
+      subroutine micro_dev(emic,target,k)
+      dimension emic(6),target(6)
+      real*8, intent(in) ::  emic, target
+      real*8 dh, cos_chi2
+
+
+      return
+      end subroutine
